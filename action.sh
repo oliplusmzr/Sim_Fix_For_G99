4d1a221e2ed57bbe0f2a03e6152e19d2d77a4d79
oliplusmzr <96580492+oliplusmzr@users.noreply.github.com>
2025-06-26 17:00:48 +0300

    Add files via upload

---
+++ action.sh
@@ -0,0 +1,33 @@
+set -x
+exec > /data/local/tmp/simfix_debug.log 2>&1
+LOGFILE="/data/local/tmp/simfix_log.txt"
+{
+  echo "====== $(date) ======"
+  echo "Manual SIM reset started"
+} >> $LOGFILE
+echo "         **** $(date) **** "
+echo " Service Started"
+sleep 1
+echo "
+╔═══╗────╔═══╦══╦═╗╔═╗─╔═╗─────╔═══╦═══╦═══╗
+║╔═╗║────║╔══╩╣╠╩╗╚╝╔╝─║╔╝─────║╔═╗║╔═╗║╔═╗║
+║╚══╦╦╗╔╗║╚══╗║║─╚╗╔╝─╔╝╚╦══╦═╗║║─╚╣╚═╝║╚═╝║
+╚══╗╠╣╚╝║║╔══╝║║─╔╝╚╗─╚╗╔╣╔╗║╔╝║║╔═╬══╗╠══╗║
+║╚═╝║║║║║║║──╔╣╠╦╝╔╗╚╗─║║║╚╝║║─║╚╩═╠══╝╠══╝║
+╚═══╩╩╩╩╝╚╝──╚══╩═╝╚═╝─╚╝╚══╩╝─╚═══╩═══╩═══╝"
+sleep 1
+echo " Version Blue"
+svc data disable
+sleep 1
+svc data enable
+stop ril-daemon
+sleep 1
+start ril-daemon
+sleep 10
+echo " Manual SIM reset completed."
+sleep 2
+echo " Feedback = > Telegram @oliplusmzr"
+sleep 1 
+echo "Manual SIM reset completed." >> $LOGFILE
+echo "" >> $LOGFILE
+log -p i -t SIM_FIX "Manual SIM reset done via ACTION button"
